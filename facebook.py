98ef162da33275a530ef7fc29ca3f037caef7a03
Hacker-heavens <74442606+Hacker-heavens@users.noreply.github.com>
2020-11-20 15:06:48 +0000

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -76,7 +76,7 @@ def welcome():
 """
 	total = open(passwordlist,"r")
 	total = total.readlines()
-	print wel 
+	print 
 	print " [💣] Account to crack : {}".format(email)
 	print " [*] Loaded :" , len(total), "passwords"
 	print " [*] Cracking, please wait ...\n\n"
